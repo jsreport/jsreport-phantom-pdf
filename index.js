f687832d4c8d8dc4f9939e6bbbc0c5acd1da3270
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2015-12-05 19:20:10 +0100

    add support for jsreport.use

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var config = require('./jsreport.config.js')
 module.exports = function (options) {
   config.options = options
   config.main = main
+  config.directory = __dirname
   return config
 }
