6a4bbe73a156d01a40561de0132ff1662266430e
taoqf <tao_qiufeng@126.com>
2018-02-05 20:15:43 +0800

    add type definition

---
+++ index.d.ts
@@ -0,0 +1,51 @@
+/// <reference types='jsreport-core' />
+
+declare namespace JsReport {
+	const enum RecipeType {
+		PhantomPdf = 'phantom-pdf'
+	}
+
+	interface Margin {
+		left: number | string;
+		right: number | string;
+		top: number | string;
+		bottom: number | string;
+	}
+
+	interface Phantom {
+		margin: string | Margin;
+		header: string;
+		footer: string;
+		width: string;
+		height: string;
+		headerHeight: string;
+		footerHeight: string;
+		format: string;
+		orientation: 'portrait' | 'landscape';
+		blockJavaScript: 'true' | any;
+		resourceTimeout: number;
+		waitForJS: 'true' | 'false';
+		fitToPage: 'true' | 'false';
+		customPhantomJS: 'true' | 'false';
+		phantomjsVersion: string;
+	}
+
+	interface Template {
+		phantom?: Partial<Phantom>;
+	}
+}
+
+declare namespace JsReportPhantomPdf {
+	interface Options {
+		allowLocalFilesAccess: boolean;
+		appDirectory: string;
+		defaultPhantomjsVersion: string;
+		strategy: string;
+	}
+}
+
+declare function JsReportPhantomPdf(options?: Partial<JsReportPhantomPdf.Options>): JsReport.Recipe;
+
+declare module 'jsreport-phantom-pdf' {
+	export = JsReportPhantomPdf;
+}
