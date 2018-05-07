923cf4b86aabe892b7080a28da5b6a894f5e3e57
BJR Matos <bjrmatos@gmail.com>
2018-05-06 20:19:17 -0500

    add optionsSchema

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,6 +1,27 @@
+
+const schema = {
+  type: 'object',
+  properties: {
+    strategy: { type: 'string', enum: ['dedicated-process', 'phantom-server'] },
+    numberOfWorkers: { type: 'number' },
+    timeout: { type: 'number' },
+    allowLocalFilesAccess: { type: 'boolean' },
+    defaultPhantomjsVersion: { type: 'string' },
+    host: { type: 'string' },
+    portLeftBoundary: { type: 'number' },
+    portRightBoundary: { type: 'number' }
+  }
+}
+
 module.exports = {
   'name': 'phantom-pdf',
   'main': 'lib/phantom.js',
-  'dependencies': [ 'templates' ],
+  'dependencies': ['templates'],
+  'optionsSchema': {
+    phantom: { ...schema },
+    extensions: {
+      'phantom-pdf': { ...schema }
+    }
+  },
   'embeddedSupport': true
 }
